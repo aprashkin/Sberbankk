47b0b36fbb8e413f16eabb1dfec92b0091357baf
aprAshkin <aprAshkin@DESKTOP-V1QV72U>
2023-10-11 16:39:55 +0300

    Добавьте файлы проекта.

---
+++ Sberbankk/Program.cs
@@ -0,0 +1,138 @@
+﻿using sberbankk;
+
+
+Random random = new Random();
+
+
+string hello = @"
+                                                                                           
+          _._._                       _._._
+         _|   |_                     _|   |_
+         | ... |_._._._._._._._._._._| ... |
+         | ||| |  o   SBERBANK    o  | ||| |
+         | """""" |  """"""    """"""    """"""  | """""" |
+    ())  |[-|-]| [-|-]  [-|-]  [-|-] |[-|-]|  ()
+    ())) |     |---------------------|     | (())
+  (())())| """""" |  """"""    """"""    """"""  | """""" |(())())
+  (()))()|[-|-]|  :::   .-""-.   :::  |[-|-]|(()))()
+  ()))(()|     | |~|~|  |_|_|  |~|~| |     |()))(()
+     ||  |_____|_|_|_|__|_|_|__|_|_|_|_____|  ||
+  ~ ~^^ @@@@@@@@@@@@@@/=======\@@@@@@@@@@@@@@ ^^~ ~
+      ^~^~                                ~^~^
+
+                                                                                                                                                                                                                                                                                     
+";
+Console.WriteLine(hello + "\nДобро пожаловать в Сбербанк! Оператор Даниил на связи!\n ");
+
+
+
+
+
+
+int Number = 1;
+string fio = "IVANOV IVAN IVANOVICH";
+float balance = 23523;
+
+int shetNumber2 = 2;
+string fio2 = "PETROV PETR PETROVICH";
+float balance2 = 13422;
+
+
+Bank[] Schet = new Bank[3];
+Schet[0] = new Bank();
+Schet[1] = new Bank();
+Schet[2] = new Bank();
+
+Console.WriteLine("Открываем счет...");
+Console.Write("Введите ваше ФИО: ");
+int shetNumber3 = 3;
+string fio3 = Console.ReadLine();
+float balance3 = 0;
+
+Schet[0].open(Number, fio, balance);
+Schet[1].open(shetNumber2, fio2, balance2);
+Schet[2].open(shetNumber3, fio3, balance3);
+
+
+
+
+
+
+
+Console.WriteLine("Все счета в нашем банке: ");
+
+for (int i = 0; i < Schet.Length; i++)
+{
+    Console.Write(i + 1 + " - ");
+    Schet[i].Out();
+}
+
+Console.Write("\nВведите номер личного счета пользователя: ");
+int id = int.Parse(Console.ReadLine()) - 1;
+
+Console.Clear();
+
+
+
+
+    Console.WriteLine("\n\nВыберите действие:\n1 - показать информацию \n2 - положить деньги на счёт\n3 - снять деньги со счёта\n4 - Обналичить счёт полностью\n5 - Перевести клиенту банка");
+    Console.Write("Ввод: ");
+    int choise = int.Parse(Console.ReadLine());
+
+    if (choise == 2)
+    {
+        Console.Clear();
+        Schet[id].Out();
+
+    }
+
+    if (choise == 3)
+    {
+        Console.Clear();
+        Console.WriteLine("Пополнение счета");
+        Console.Write("Положите купюры в купюроприемник (до 200 шт.): ");
+        float money = float.Parse(Console.ReadLine());
+       
+        Schet[id].popolnenie(money);
+    }
+
+    if (choise == 4)
+    {
+        Console.Clear();
+        Console.WriteLine("Снятие денег со счета.");
+        Console.Write("Введите сумму: ");
+        float money = float.Parse(Console.ReadLine());
+        
+        Schet[id].spisanie(money);
+    }
+
+    if (choise == 5)
+    {
+        Console.Clear();
+        Console.WriteLine("Обналичивание всего счета.");
+        Schet[id].obnul();
+        Schet[id].Out();
+    }
+
+    if (choise == 6)
+    {
+        Console.Clear();
+        for (int i = 0; i < Schet.Length; i++)
+        {
+            Console.Write(i + 1 + " - ");
+            Schet[i].Out();
+        }
+        Console.Write("Перевод с вашего счёта на счет другого клиента банка.\nВведите айди клиента на который хотите сделать перевод: ");
+
+        int id2 = int.Parse(Console.ReadLine()) - 1;
+        Console.Write("Введите сумму: ");
+        float money = float.Parse(Console.ReadLine());
+       
+        Console.WriteLine("\nИнформация о вашем счёте.");
+        Schet[id].spisanie(money);
+        Console.WriteLine("\nИнформация счета клиента");
+        Schet[id2].popolnenie(money);
+
+    }
+
+//все равно рекламы не будет. саша прости
---
+++ Sberbankk/bank.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sberbankk
+{
+    internal class Bank
+    {
+        private int nom;
+        private string name;
+        private float sum;
+
+        public void open(int nom, string name, float sum)
+        {
+            this.nom = nom;
+            this.name = name;
+            this.sum = sum;
+        }
+
+        public void Out()
+        {
+            Console.WriteLine($"Номер счёта: {nom}. ФИО: {name}.Сумма: {sum}.");
+        }
+
+        public void popolnenie(float money)
+        {
+            Console.WriteLine("Вы успешно добавили на счёт " + money);
+            sum += money;
+            Console.WriteLine($"Ваш баланс: {sum}");
+        }
+
+        public void spisanie(float money)
+        {
+            Console.WriteLine("Вы успешно сняли со счёта " + money);
+            sum -= money;
+            if (sum < 0)
+            {
+                Console.WriteLine("Ваш баланс отрицателен. За вами уже едут, я договорился");
+            }
+            Console.WriteLine($"Ваш баланс: {sum}");
+        }
+
+        public void obnul()
+        {
+            Console.WriteLine("Вы успешно сняли всю сумму.\nС вашего счёта списано " + sum);
+            sum = 0;
+        }
+
+        public void perevod(float money)
+        {
+            Console.WriteLine($"Вы успешно перевели {money} на счет другого клиента");
+            if (sum < 0)
+            {
+                Console.WriteLine("Ваш баланс отрицательный. За вами уже едут, я договорился");
+            }
+
+        }
+
+    }
+    //рекламы не будет. сбербанк не одобряет
+
+}
