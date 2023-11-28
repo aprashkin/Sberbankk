b4ef5b1beb7cffdb165bffd2c88ca9bc76e6cc50
sharova_anastasiya <anastasiysharova1910@gmail.com>
2023-11-28 13:18:47 +0300

    .

--- Sberbankk/Program.cs
+++ Sberbankk/Program.cs
@@ -1,10 +1,13 @@
-﻿using sberbankk;
+﻿
 
+using System;
+using sberbankk;
+class Program
 
-Random random = new Random();
-
-
-string hello = @"
+{
+    static void Main()
+    {
+        string hello = @"
                                                                                            
           _._._                       _._._
          _|   |_                     _|   |_
@@ -22,108 +25,97 @@ string hello = @"
 
                                                                                                                                                                                                                                                                                      
 ";
-Console.WriteLine(hello + "\nДобро пожаловать в Сбербанк! Оператор Даниил на связи!\n ");
+        Console.WriteLine(hello + "\nДобро пожаловать в Сбербанк! Оператор Даниил на связи!\n ");
 
+        
 
+        Console.Write("Введите количество счетов, которые вы хотите открыть: ");
+        int numberOfAccounts = int.Parse(Console.ReadLine());
 
+        Bank[] Schet = new Bank[numberOfAccounts];
 
-int Number = 1;
-string fio = "IVANOV IVAN IVANOVICH";
-float balance = random.Next(0, 100000);
-
-int shetNumber2 = 2;
-string fio2 = "PETROV PETR PETROVICH";
-float balance2 = random.Next(0, 100000);
-
-
-Bank[] Schet = new Bank[3];
-Schet[0] = new Bank();
-Schet[1] = new Bank();
-Schet[2] = new Bank();
-
-Console.WriteLine("Открываем счет...");
-Console.Write("Введите ваше ФИО: ");
-int shetNumber3 = 3;
-string fio3 = Console.ReadLine();
-float balance3 = 0;
-
-Schet[0].open(Number, fio, balance);
-Schet[1].open(shetNumber2, fio2, balance2);
-Schet[2].open(shetNumber3, fio3, balance3);
-
-    
-    
-while (true)
-{
-    Console.WriteLine("Все счета в нашем банке: ");
-    for (int i = 0; i < Schet.Length; i++)
-    {
-        Console.Write(i + 1 + " - ");
-        Schet[i].Out();
-    }
-
-    Console.Write("\nВведите номер личного счета пользователя: ");
-    int id = int.Parse(Console.ReadLine()) - 1;
-
-    Console.Clear(); 
-    
-    Console.WriteLine(
-        "\n\nВыберите действие:\n1 - показать информацию \n2 - положить деньги на счёт\n3 - снять деньги со счёта\n4 - Обналичить счёт полностью\n5 - Перевести клиенту банка");
-    Console.Write("Ввод: ");
-    int choise = int.Parse(Console.ReadLine());
-
-    if (choise == 1)
-    {
-        Console.Clear();
-        Schet[id].Out();
-
-    }
-
-    if (choise == 2)
-    {
-        Console.Clear();
-        Console.WriteLine("Пополнение счета");
-        Console.Write("Положите купюры в купюроприемник (до 200 шт.): ");
-        float money = float.Parse(Console.ReadLine());
-
-        Schet[id].popolnenie(money);
-    }
-
-    if (choise == 3)
-    {
-        Console.Clear();
-        Console.WriteLine("Снятие денег со счета.");
-        Console.Write("Введите сумму: ");
-        float money = float.Parse(Console.ReadLine());
-
-        Schet[id].spisanie(money);
-    }
-
-    if (choise == 4)
-    {
-        Console.Clear();
-        Console.WriteLine("Обналичивание всего счета.");
-        Schet[id].obnul();
-        Schet[id].Out();
-    }
-
-    if (choise == 5)
-    {
-        Console.Clear();
-        for (int i = 0; i < Schet.Length; i++)
+        for (int i = 0; i < numberOfAccounts; i++)
         {
-            Console.Write(i + 1 + " - ");
-            Schet[i].Out();
+            Console.WriteLine($"\nОткрываем счет {i + 1}...");
+            Console.Write("Введите ваше ФИО: ");
+            string fio = Console.ReadLine();
+            Console.Write("Введите начальный баланс: ");
+            float balance = float.Parse(Console.ReadLine());
+
+            Schet[i] = new Bank();
+            Schet[i].open(i + 1, fio, balance);
         }
 
-        Console.Write("Перевод с вашего счёта на счет другого клиента банка.\nВведите айди клиента на который хотите сделать перевод: ");
+        Console.Write("Введите айди клиента для входа: ");
+        int currentUserId = int.Parse(Console.ReadLine()) - 1;
+        
+        
 
-        int id2 = int.Parse(Console.ReadLine()) - 1;
-        Console.Write("Введите сумму: ");
-        float money = float.Parse(Console.ReadLine());
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("\nВсе счета в нашем банке: ");
+            for (int i = 0; i < Schet.Length; i++)
+            {
+                Console.Write($"{i + 1} - ");
+                Schet[i].Out();
+            }
 
-        Schet[id].perevod(money, Schet[id2]);
+            Console.WriteLine($"\nВы вошли под пользователем {currentUserId + 1}.");
+            Console.WriteLine("\nВыберите действие:\n1 - показать информацию\n2 - положить деньги на счёт\n3 - снять деньги со счёта\n4 - Обналичить счёт полностью\n5 - Перевести клиенту банка\n6 - Сменить пользователя\n7 - Выйти из программы");
+            Console.Write("Ввод: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    Schet[currentUserId].Out();
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Пополнение счета");
+                    Console.Write("Положите купюры в купюроприемник: ");
+                    float money = float.Parse(Console.ReadLine());
+                    Schet[currentUserId].popolnenie(money);
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Снятие денег со счета.");
+                    Console.Write("Введите сумму: ");
+                    money = float.Parse(Console.ReadLine());
+                    Schet[currentUserId].spisanie(money);
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("Обналичивание всего счета.");
+                    Schet[currentUserId].obnul();
+                    Schet[currentUserId].Out();
+                    break;
+                case 5:
+                    Console.Clear();
+                    for (int i = 0; i < Schet.Length; i++)
+                    {
+                        Console.Write($"{i + 1} - ");
+                        Schet[i].Out();
+                    }
+                    Console.Write("Перевод с вашего счёта на счет другого клиента банка.\nВведите айди клиента на который хотите сделать перевод: ");
+                    int id2 = int.Parse(Console.ReadLine()) - 1;
+                    Console.Write("Введите сумму: ");
+                    money = float.Parse(Console.ReadLine());
+                    Schet[currentUserId].perevod(money, Schet[id2]);
+                    break;
+                case 6:
+                    Console.Write("Введите айди клиента для смены пользователя: ");
+                    currentUserId = int.Parse(Console.ReadLine()) - 1;
+                    break;
+                case 7:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+                    break;
+            }
+        }
     }
-
 }
-//ПРОДАМ ГАРАЖ 25 М.КВ. 
